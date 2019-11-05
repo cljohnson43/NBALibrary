e747aebb890663f03b064250500a4d1840ba5565
Chris Johnson <cljohnson4343@gmail.com>
2019-11-04 19:21:14 -0600

    Load all players from firebase.

--- app/src/main/java/com/example/nbalibrary/MainActivity.kt
+++ app/src/main/java/com/example/nbalibrary/MainActivity.kt
@@ -2,11 +2,14 @@ package com.example.nbalibrary
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.nbalibrary.data.PlayersRepo
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        PlayersRepo().loadPlayers()
     }
 }
--- app/src/main/java/com/example/nbalibrary/data/PlayersRepo.kt
+++ app/src/main/java/com/example/nbalibrary/data/PlayersRepo.kt
@@ -1,4 +1,27 @@
 package com.example.nbalibrary.data
 
+import android.util.Log
+import com.google.firebase.database.DataSnapshot
+import com.google.firebase.database.DatabaseError
+import com.google.firebase.database.FirebaseDatabase
+import com.google.firebase.database.ValueEventListener
+
+private const val TAG = "PlayersRepo"
+
 class PlayersRepo {
+
+    fun loadPlayers() {
+        val db = FirebaseDatabase.getInstance().reference
+
+        db.addListenerForSingleValueEvent(object: ValueEventListener{
+            override fun onCancelled(p0: DatabaseError) {
+            }
+
+            override fun onDataChange(snapshot: DataSnapshot) {
+                Log.d(TAG, "DB has ${snapshot.childrenCount} children")
+            }
+        })
+
+    }
+
 }
